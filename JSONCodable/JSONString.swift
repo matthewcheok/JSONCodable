506c82ad5348c17be79c461000bb48fa09f568df
Eungkyu Song <eungkyu@gmail.com>
2015-09-29 04:22:57 +0900

    Support primitive type in toJSONString

--- JSONCodable/JSONString.swift
+++ JSONCodable/JSONString.swift
@@ -10,12 +10,48 @@ import Foundation
 
 public extension JSONEncodable {
     public func toJSONString() throws -> String {
-        let json = try toJSON()
-        let data = try NSJSONSerialization.dataWithJSONObject(json, options: NSJSONWritingOptions(rawValue: 0))
-        guard let string = NSString(data: data, encoding: NSUTF8StringEncoding) else {
-            return ""
+        switch self {
+        case let str as String:
+            return escapeJSONString(str)
+        case is Bool, is Int, is Float, is Double:
+            return String(self)
+        default:
+            let json = try toJSON()
+            let data = try NSJSONSerialization.dataWithJSONObject(json, options: NSJSONWritingOptions(rawValue: 0))
+            guard let string = NSString(data: data, encoding: NSUTF8StringEncoding) else {
+                return ""
+            }
+            return string as String
         }
-        return string as String
+    }
+}
+
+private func escapeJSONString(str: String) -> String {
+    var chars = String.CharacterView()
+    for c in str.characters {
+        switch c {
+        case "\\":
+            chars.append("\\")
+            chars.append("\\")
+        case "\"":
+            chars.append("\\")
+            chars.append("\"")
+        default:
+            chars.append(c)
+        }
+    }
+    chars.append("\"")
+    return String(chars)
+}
+
+public extension Optional where Wrapped: JSONEncodable {
+    public func toJSONString() throws -> String {
+        switch self {
+        case let .Some(jsonEncodable):
+            return try jsonEncodable.toJSONString()
+        case nil:
+            return "null"
+        }
     }
 }
 
